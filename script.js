bcb4d36a51a6e92e9fed03fe20f3313abc5367c0
Olaitan Adedokun <kenzdex@gmail.com>
2025-06-10 21:26:55 +0100

    input retrieval and trimming

--- script.js
+++ script.js
@@ -6,5 +6,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
   form.addEventListener("submit", function (event) {
     event.preventDefault();
+
+    const username = document.getElementById("username").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    console.log(username.value);
   });
 });
