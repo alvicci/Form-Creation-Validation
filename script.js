1305b9470ab7361ad5b55bdabfde2be7742f9fb7
Olaitan Adedokun <kenzdex@gmail.com>
2025-06-10 21:07:16 +0100

    setup initial code structure

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 "use script";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("registration-form");
+  const feedbackDiv = document.getElementById("form-feedback");
+});
